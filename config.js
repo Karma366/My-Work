f320ce507d11bcd92679e2edd9408927303f7196
Karma366 <sangaydendi@gmail.com>
2026-08-19 15:02:32 +0600

    Update SHEET_URL to point to the live script

--- config.js
+++ config.js
@@ -5,4 +5,4 @@
 // only in this browser (they'll disappear if you clear browser data).
 // ──────────────────────────────────────────────────────────────────────
 
-const SHEET_URL = "";
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbwQ0O5lCUv-35eBIXPDMvOz6BD8bHL3l8KnB81r2rBZuh-ZsvalUswZI40gU323SSwZ/exec";
